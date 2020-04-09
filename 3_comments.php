1d96e782fa38d1b8bebbbf690cc9965db115d1bb
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-04-09 22:44:46 +0100

    adicionei descrição de como comentar código

---
+++ 3_comments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Comentários</title>
+</head>
+<body>
+    
+<?php 
+
+    // Antes de avançar mais, há que esclarecer como comentar código em php
+    // uma linha apenas de código  
+
+    /*
+    * Multiplas
+    linhas 
+    de
+    código
+    */
+
+    echo "Hello World"; //Olá mundo!!!
+?> 
+    
+</body>
+    <!-- Já agora aproveito para deixar como comentar conteudo HTML -->
+</html>
