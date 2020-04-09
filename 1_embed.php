396ef7b4bdad2e1aad265cd99e8584cc58581949
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-04-09 22:23:28 +0100

    adicionei uma descrição sobre o ficheiro

---
+++ 1_embed.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Informação pertinente:
+ * A linguagem php é Top-Down, o que significa que lê o código de cima para baixo.
+ * Para escrever código PHP é precio abrir as TAGs especificas
+ * 
+ * <?php - para abrir 
+ * ?> - para fechar       
+ * 
+ * No entanto, segundo as PSRs (Php Standard Recomendations) e também compatibilidade com todos os servidores Apache, convém
+ * usar a nomenclatura completa em vez da curta, por questões de interpretação
+ * 
+ * Os ficheiros tem que ser codificados com UTF-8
+ * 
+ * É DE relembrar que os ficheiros php
+ * DEVEM declarar funções, classes ou constantes ou causar efeitos como fazer echos ou includes.
+ * Mas segundo as PSRs, nunca os dois simultaneamente 
+ * 
+ * ficheiros php não devem ter tabulação mas sim 4 espaços para identar e boas práticas recomendam deixar a ultima linha em branco
+ * * não sei porque, mas pronto.
+ * 
+ * E pronto aqui está o nosso primeiro ficheiro php
+ *  
+ */
+
+echo "Hello World"; //comando echo vai imprimir no ecrã a frase Hello World
