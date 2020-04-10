5726038df01fc9099a3970b254a563fc1a8153a6
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-04-10 15:44:45 +0100

    ficheiros com exemplos matemáticos

---
+++ 5_math.php
@@ -0,0 +1,27 @@
+<?php
+
+    $soma = 56 + 45; //soma
+    $diference = 56 - 45; //diferença
+    $mutiplication = 56 * 45; //multiplicação
+    $division = 56 / 45; //divisão
+    echo "Soma: 56 + 45 = " . $soma; 
+    echo "<br>";
+    echo "Diferença 56 - 45 = " . $diference; 
+    echo "<br>";
+    echo "Multiplicação 56 * 45 = " . $mutiplication; 
+    echo "<br>";
+    echo "Devisão 56 / 45 = " . $division;
+    echo "<br>"; 
+    echo "<br>";
+
+    //Outros exemplo de como implementar somas sem necessidade de uma variavel
+
+    echo 45 + 34 * 45 /421 - 45;
+    echo "<br>";
+    echo 5 + 5 / 10;
+    echo "<br>";
+    //mais um exemplo
+    $number1 = 12;
+    $number2 = 24;
+
+    echo $number1 * $number2;
